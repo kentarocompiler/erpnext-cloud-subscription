72b6991935499845f989b59da1d5ca31bc09953c
kentaro <kentarocompiler@gmail.com>
2026-07-05 23:05:30 +0700

    สร้าง Doctype Cloud Server Subscription จากโน้ตบุ๊ก

---
+++ trio/trio/doctype/cloud_server_subscription/cloud_server_subscription.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, kentaro and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Cloud Server Subscription", {
+// 	refresh(frm) {
+
+// 	},
+// });
